615b46ea0d40730f13ed4961e6a9db96c517eec5
AlexBraginets <qetting@gmail.com>
2023-05-11 22:37:54 +0300

    errors cleanup

--- Assets/Scripts/BallsWayDataRefresher.cs
+++ Assets/Scripts/BallsWayDataRefresher.cs
@@ -4,16 +4,22 @@ public class BallsWayDataRefresher : MonoBehaviour
 {
     [SerializeField] private WayDataHolder[] _wayDataHolders;
     [SerializeField] private PipePosDataProvider[] _pipePosDataProviders;
+
     public void Refresh(int wayIndex, Ball[] balls)
     {
         GetPosData(wayIndex, out PosData[] data);
         for (int i = 0; i < balls.Length; i++)
         {
             var ball = balls[i];
-            ball.UpdateLines(_wayDataHolders[wayIndex].Lines);
-            ball.UpdatePosData(data[i]);
+            ball.UpdateWayData(new WayData()
+            {
+                Lines = _wayDataHolders[wayIndex].Lines,
+                LineIndex = data[i].LineIndex,
+                LocalLength = data[i].LocalLength
+            });
         }
     }
+
     private void GetPosData(int wayIndex, out PosData[] data)
     {
         var posDataProvider = _pipePosDataProviders[wayIndex];
