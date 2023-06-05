2d7fb8da856be6c176cb1f80a8a84acde3c804a8
AlexBraginets <qetting@gmail.com>
2023-06-05 19:02:00 +0300

    save

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,7 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         _tubeSwitcher.OnSwitchedStatic += CheckWin;
+        Application.targetFrameRate = 60;
     }
 
     private void CheckWin()
